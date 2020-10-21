e8c434fdf3630f750f51b96365b1c8c5e45f4a8c
bkaced-admin-local <kacedbouz@gmail.com>
2020-10-21 13:10:28 -0400

    Déplacement Data dans Model et supprimer ce dossier.
    Modification des namespace:
    1 - Data\migration et ApplicationDbContext
    2- namespace ClientProduitDevExpress.Data.Migrations
    3 - Startup: pointer vers using ClientProduitDevExpress.BibliothequeModels.Data;
    4 - installation et utilisation des bibliothéque using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
    using Microsoft.EntityFrameworkCore; dans BibliothequeModels

--- BibliothequeModels/Data/ApplicationDbContext.cs
+++ BibliothequeModels/Data/ApplicationDbContext.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace ClientProduitDevExpress.Data
+
+namespace  ClientProduitDevExpress.BibliothequeModels.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+           
         }
     }
 }
--- ClientProduitDevExpress/Startup.cs
+++ ClientProduitDevExpress/Startup.cs
@@ -8,10 +8,11 @@ using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
-using ClientProduitDevExpress.Data;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ClientProduitDevExpress.BibliothequeModels.Data;
 
 namespace ClientProduitDevExpress
 {
