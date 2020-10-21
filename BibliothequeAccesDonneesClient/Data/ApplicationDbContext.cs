45c9dc7c07c85c1770a7177526f1e1c43b5441cc
bkaced-admin-local <kacedbouz@gmail.com>
2020-10-21 13:22:11 -0400

    Déplacement Data dans BibliothequeAccesDonneesClient et supprimer ce dossier.
    Modification des namespace:
    1 - Data\migration et ApplicationDbContext
    2- namespace ClientProduitDevExpress.Data.Migrations
    3 - Startup: pointer vers using ClientProduitDevExpress.BibliothequeModels.Data;
    4 - installation et utilisation des bibliothéque using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
    using Microsoft.EntityFrameworkCore; dans BibliothequeModels

--- BibliothequeAccesDonneesClient/Data/ApplicationDbContext.cs
+++ BibliothequeAccesDonneesClient/Data/ApplicationDbContext.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Text;
 
 
-namespace  ClientProduitDevExpress.BibliothequeModels.Data
+namespace  ClientProduitDevExpress.BibliothequeAccesDonneesClient.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- BibliothequeAccesDonneesClient/Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ BibliothequeAccesDonneesClient/Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace ClientProduitDevExpress.Data.Migrations
+namespace ClientProduitDevExpress.BibliothequeAccesDonneesClient.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
